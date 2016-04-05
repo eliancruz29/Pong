2338baef6eefe5451eb1cbc7b9181547ce100b07
eliancruz29 <eliancruz29@gmail.com>
2016-04-05 01:17:46 -0400

    Modificaciones para poder mover los dos jugadores

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+var padding = 20;
+var paddingInit = 200;
+var velocity = 600;
 
 var HelloWorldLayer = cc.Layer.extend({
     jugador1:null,    
@@ -6,9 +9,16 @@ var HelloWorldLayer = cc.Layer.extend({
     puntuacion1:null,
     puntuacion2:null,
     sizeW:null,
+    cuadro: null,
     inicializar:function(){
         var size = cc.winSize;
         this.sizeW = size;
+        this.cuadro = {
+            left: 0+padding,
+            right: this.sizeW.width-padding,
+            top: this.sizeW.height,
+            bottom: 0
+        }
         var color = cc.color(100,100,100);
 
         this.jugador1 =  new cc.DrawNode();
@@ -50,20 +60,20 @@ var HelloWorldLayer = cc.Layer.extend({
             if(direction.toLowerCase() == "left"){
                 var height = 0;
                 if(this.random(0, 1))
-                    height = this.sizeW.height;
+                    height = this.cuadro.top;
                 action = cc.moveTo(5, cc.p(0, height));
             }else{
                 var height = 0;
                 if(this.random(0, 1))
-                    height = this.sizeW.height;
-                action = cc.moveTo(5, cc.p(this.sizeW.width, height));
+                    height = this.cuadro.top;
+                action = cc.moveTo(5, cc.p(this.cuadro.right, height));
             }
         }else{
             var height = 0, width = 0;
             if(this.random(0, 1))
-                height = this.sizeW.height;
+                height = this.cuadro.top;
             if(this.random(0, 1))
-                width = this.sizeW.width;
+                width = this.cuadro.right;
             action = cc.moveTo(5, cc.p(width, height));
         }
         this.pelota.runAction(action);
@@ -72,12 +82,81 @@ var HelloWorldLayer = cc.Layer.extend({
     verifyCollisionPelota: function(){
         console.log("Tamo aki");
     },
+        
+    calculeVelocity: function(distance){
+        distance = Math.abs(distance);
+        return distance/velocity;
+    },
+    
+    moveLimites: function(keyCode, event){
+        var juego = event.getCurrentTarget();
+        var top = juego.cuadro.top;
+        var bottom = juego.cuadro.bottom;
+        switch (keyCode){
+        case 38:
+                var distance = top - juego.jugador1.getPositionY() - 100;
+                if(!juego.jugador1.getActionByTag(101)){
+                    var action = cc.moveTo(juego.calculeVelocity(distance), cc.p(juego.jugador1.getPositionX(), (top-100)));
+                    action.setTag(101);
+                    juego.jugador1.runAction(action);
+                }
+            break;
+        case 40:
+                var distance = juego.jugador1.getPositionY() - bottom + 100;
+                if(!juego.jugador1.getActionByTag(101)){
+                    var action = cc.moveTo(juego.calculeVelocity(distance), cc.p(juego.jugador1.getPositionX(), 0));
+                    action.setTag(101);
+                    juego.jugador1.runAction(action);
+                }
+            break;
+        case 65:
+                var distance = top - juego.jugador2.getPositionY() - 100;
+                if(!juego.jugador2.getActionByTag(102)){
+                    var action = cc.moveTo(juego.calculeVelocity(distance), cc.p(juego.jugador2.getPositionX(), (top-100)));
+                    action.setTag(102);
+                    juego.jugador2.runAction(action);
+                }
+            break;
+        case 90:
+                var distance = juego.jugador2.getPositionY() - bottom + 100;
+                if(!juego.jugador2.getActionByTag(102)){
+                    var action = cc.moveTo(juego.calculeVelocity(distance), cc.p(juego.jugador2.getPositionX(), 0));
+                    action.setTag(102);
+                    juego.jugador2.runAction(action);
+                }
+            break;
+        }
+    },
+    
+    stopLimites: function(keyCode, event){
+        var juego = event.getCurrentTarget();
+        switch (keyCode){
+        case 38:
+        case 40:
+                var jugador = juego.jugador1;
+                jugador.stopActionByTag(101);
+            break;
+        case 65:
+        case 90:
+                var jugador = juego.jugador2;
+                jugador.stopActionByTag(102);
+            break;
+        }
+    },
     
     ctor:function () {
         this._super();
         this.inicializar();
         
         this.scheduleOnce(this.movePelota, 1);
+        //this.schedule(this.verifyCollisionPelota, null, null, 1);
+        
+        //add a keyboard event listener to statusLabel
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: this.moveLimites,
+            onKeyReleased: this.stopLimites
+        }, this);
         
         return true;
     }
